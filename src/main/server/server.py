9193ccee38551d51e5a80bbdd212ccec983aa8db
Heithor Urquiza <111160912+HeithorUrquiza@users.noreply.github.com>
2024-07-09 13:02:30 -0300

    config: adding first test route to project

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from src.main.routes.trips_routes import trips_routes_bp
 
 # Creating a server with Flask
 app = Flask(__name__)
+
+# Registering routes in the server
+app.register_blueprint(trips_routes_bp)
